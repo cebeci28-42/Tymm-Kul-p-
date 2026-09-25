1efa1cfe2534c2bec6438c7b8077c193be14fb7e
cebeci28-42 <cigdemcebeci42@gmail.com>
2026-09-25 23:57:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
-const CACHE='klup-dosyasi-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon192.png','./icon512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+const CACHE='klup-dosyasi-v4';
+const APP=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
+  if(e.request.method!=='GET')return;
   e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{
-    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+    if(r && r.ok && new URL(e.request.url).protocol.startsWith('http')){
+      const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+    }
+    return r;
   }).catch(()=>caches.match('./index.html'))));
 });
