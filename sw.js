846e288ddb5733390cc1af9c14bd96f60dbea610
cebeci28-42 <cigdemcebeci42@gmail.com>
2026-09-25 23:25:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='klup-dosyasi-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const ASSETS=['./','./index.html','./manifest.json','./icon192.png','./icon512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>{
